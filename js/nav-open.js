bca0498226832a1660b915f274bb26fb16803131
Aidar <mrsalamon77@gmail.com>
2019-09-07 21:45:16 +0300

    Исправлено меню на мобильном

--- js/nav-open.js
+++ js/nav-open.js
@@ -2,9 +2,12 @@ var nav = document.querySelector(".site-list");
 var toggle = document.querySelector(".main-nav__toggle");
 
 toggle.addEventListener("click", function() {
+	console.log("Работает");
 	if(nav.classList.contains("site-list--active")) {
 		nav.classList.remove("site-list--active");
+		console.log("Удалить");
 	} else {
 		nav.classList.add("site-list--active");
+		console.log("Добавить");
 	}
 });
